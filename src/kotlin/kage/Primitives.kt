a00c44ff864b6632a66a1a00e3f53e95ae4425e8
Aditya Wasan <adityawasan55@gmail.com>
2021-09-25 13:29:50 +0530

    feat(primitives): Add `Primitives` class
    This class includes the encrypt and decrypt methods for one-time use keys.
    
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ src/kotlin/kage/Primitives.kt
@@ -0,0 +1,43 @@
+package kage
+
+import kage.exception.EncryptionException
+import kage.exception.IncorrectCipherTextException
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+
+public object Primitives {
+    private const val ENCRYPTION_ALGO = "ChaCha20-Poly1305"
+    private const val KEY_ALGO = "ChaCha20"
+    private const val NONCE_LENGTH = 12 // 96 bits, 12 bytes
+    private const val TAG_LENGTH = 16
+
+
+    public fun aeadEncrypt(key: ByteArray, plainText: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(ENCRYPTION_ALGO)
+        // Initialize parameter spec with empty byte array
+        val parameterSpec = IvParameterSpec(ByteArray(NONCE_LENGTH))
+        // Create secretKey from byte array
+        val secretKey = SecretKeySpec(key, KEY_ALGO)
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec)
+        return cipher.doFinal(plainText) ?: throw EncryptionException("Failed to encrypt data, returned ByteArray was null")
+    }
+
+    public fun aeadDecrypt(key: ByteArray, size: Int, cipherText: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(ENCRYPTION_ALGO)
+        // Initialize parameter spec with empty byte array
+        val parameterSpec = IvParameterSpec(ByteArray(NONCE_LENGTH))
+        // Create secretKey from byte array
+        val secretKey = SecretKeySpec(key, KEY_ALGO)
+
+        if (cipherText.size != size + TAG_LENGTH) throw IncorrectCipherTextException("Encrypted value has unexpected length")
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec)
+        return cipher.doFinal(cipherText) ?: throw EncryptionException("Failed to decrypt data, returned ByteArray was null")
+    }
+
+
+}
---
+++ src/kotlin/kage/exception/CryptoException.kt
@@ -0,0 +1,29 @@
+package kage.exception
+
+public sealed class CryptoException
+@JvmOverloads
+constructor(
+  message: String? = null,
+  cause: Throwable? = null,
+) : Exception(message, cause)
+
+public class EncryptionException
+@JvmOverloads
+constructor(
+  message: String? = null,
+  cause: Throwable? = null,
+) : CryptoException(message, cause)
+
+public class DecryptionException
+@JvmOverloads
+constructor(
+  message: String? = null,
+  cause: Throwable? = null,
+) : ParseException(message, cause)
+
+public class IncorrectCipherTextException
+@JvmOverloads
+constructor(
+  message: String? = null,
+  cause: Throwable? = null,
+) : ParseException(message, cause)
